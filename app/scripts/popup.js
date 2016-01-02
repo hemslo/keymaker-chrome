f92e218c183f375e3c03ba91e808a3a3d3fd89b8
Hemslo Wang <hemslo.wang@gmail.com>
2016-01-03 00:53:52 +0800

    Add basic popup

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('app'));
